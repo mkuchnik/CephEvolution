12e64543b2329b6fb8f1f5850dd5acbf66d0ec14
Michael Kuchnik <michaelkuchnik@gmail.com>
2019-08-11 00:10:59 -0600

    Clean up rbd bench scripts

--- Cluster_Bench_Scripts/ec_rbd_bench.sh
+++ Cluster_Bench_Scripts/ec_rbd_bench.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+# Runs the RBD tests on a erasure coded RBD device
+
 filename="${1}"
 iodepth=256
-size="100M"
+size="30G"
 mkdir ec_rbd-tests
 #for rw in "write" "randwrite" "read" "randread"; do
 for rw in "randwrite"; do
+#for i in 128 64 32 16 8 4; do
 for i in 4; do
   bs="${i}K"
   echo "bs ${bs}"
@@ -12,6 +16,10 @@ for i in 4; do
     --rw=${rw} --norandommap --size=${size} --numjobs=1 \
     --ramp_time=None --name=rbd_test \
     --filename=${filename}"
-  eval ${cmd}  | tee ec_rbd-tests/${rw}-${iodepth}-${bs}-4-2-filestore.txt
+  eval ${cmd} | tee ec_rbd-tests/${rw}-${iodepth}-${bs}.txt
+  pdsh -w h[0-15] "sudo sync && sudo echo 3 | sudo tee /proc/sys/vm/drop_caches && sudo sync"
+  sleep 1s
+  pdsh -w h[0-15] "sudo systemctl restart ceph-osd.target"
+  sleep 15s
 done
 done
